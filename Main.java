73706379dafb7e30e102d233dbc85b8a4baf8169
NauH1 <souzavinicius840@gmail.com>
2025-05-13 10:43:56 -0300

    commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+
+public class Main{
+final static Scanner INPUT = new Scanner(System.in);
+    public static void main(String[] args) {
+        LocalDate data = INPUT.nextLoca
+    }
+}
