b3208c92a75bfaa67708bd3ff8e36cc9c044736c
NauH1 <souzavinicius840@gmail.com>
2025-05-13 10:57:12 -0300

    LocalDate

--- Main.java
+++ Main.java
@@ -1,10 +1,23 @@
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 
 public class Main{
 final static Scanner INPUT = new Scanner(System.in);
     public static void main(String[] args) {
-        LocalDate data = INPUT.nextLoca
+        LocalDate data = LocalDate.MAX;
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.println("Ditige uma data seguindo o padrão(dd/MM/yyyy), inclua as barras (/): ");
+        String usuarioData = INPUT.nextLine();
+        
+        try {
+            data = LocalDate.parse(usuarioData, formatador);
+            String formattedDate = data.format(formatador);
+            System.out.println(formattedDate);
+        } catch (Exception e) {
+            System.out.println("Erro!!!\n--Você digitou a data com o formato errado--");
+            main(args);
+        }
     }
 }
